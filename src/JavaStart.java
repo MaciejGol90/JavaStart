cdcfc872f4c5c6105471724c6942a9c377fd1ac9
MaciejGol90 <maciek.golianek@gmail.com>
2018-11-22 17:50:43 +0100

    JavaStart cw1

---
+++ src/JavaStart.java
@@ -0,0 +1,8 @@
+public class JavaStart {
+
+    public static void main(String[] args) {
+        System.out.println("Ania");
+        System.out.println("Bartek");
+        System.out.println("Kasia");
+    }
+}
