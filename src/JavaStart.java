7d5de6f699249e1a7faf6553278211e56a4692f5
MaciejGol90 <maciek.golianek@gmail.com>
2018-12-12 13:48:47 +0100

    zapis imienia do pliku

--- src/JavaStart.java
+++ src/JavaStart.java
@@ -1,15 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class JavaStart {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-       /* Scanner odczyt = new Scanner(System.in);
+       Scanner odczyt = new Scanner(System.in);
         System.out.println("Podaj imie");
         String imie = odczyt.nextLine();
-
+/*
         if ("Maciek".equals(imie)) {
             System.out.println("Siema " + imie);
         } else if ("Zosia".equals(imie)) {
@@ -19,10 +20,14 @@ public class JavaStart {
         }*/
 
         File imiona = new File("imiona.txt");
-        Scanner in = new Scanner(imiona);
+        Scanner in = new Scanner(imie);
 
         String zdanie = in.nextLine();
         System.out.println(zdanie);
+
+        PrintWriter zapis = new PrintWriter("imiona.txt");
+        zapis.println(imie);
+        zapis.close();
 
     }
 }
