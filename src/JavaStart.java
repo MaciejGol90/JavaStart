8f93b28158a8c1f6ead0c31edfd1698d023b1639
MaciejGol90 <maciek.golianek@gmail.com>
2018-12-12 13:26:33 +0100

    Zapis odczyt

--- src/JavaStart.java
+++ src/JavaStart.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class JavaStart {
 
     public static void main(String[] args) {
-        System.out.println("Ania");
-        System.out.println("Bartek");
-        System.out.println("Kasia");
+
+        Scanner odczyt = new Scanner(System.in);
+        System.out.println("Podaj imie");
+        String imie = odczyt.nextLine();
+
+        if ("Maciek".equals(imie)) {
+            System.out.println("Siema " + imie);
+        } else if ("Zosia".equals(imie)) {
+            System.out.println("Siema " + imie);
+        } else {
+            System.out.println("sory nie kojarze");
+        }
     }
 }
+
+
+     /*   System.out.println("Ania");
+        System.out.println("Bartek");
+        System.out.println("Kasia");
+    }*/
+
