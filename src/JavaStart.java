c011bd8baa44bdccc5b0f6b6c5016fa96f6cedb1
MaciejGol90 <maciek.golianek@gmail.com>
2018-12-12 14:13:41 +0100

    pętla odliczanie

--- src/JavaStart.java
+++ src/JavaStart.java
@@ -7,17 +7,17 @@ public class JavaStart {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-       Scanner odczyt = new Scanner(System.in);
+   /*    Scanner odczyt = new Scanner(System.in);
         System.out.println("Podaj imie");
         String imie = odczyt.nextLine();
-/*
+
         if ("Maciek".equals(imie)) {
             System.out.println("Siema " + imie);
         } else if ("Zosia".equals(imie)) {
             System.out.println("Siema " + imie);
         } else {
             System.out.println("sory nie kojarze");
-        }*/
+
 
         File imiona = new File("imiona.txt");
         Scanner in = new Scanner(imie);
@@ -28,9 +28,19 @@ public class JavaStart {
         PrintWriter zapis = new PrintWriter("imiona.txt");
         zapis.println(imie);
         zapis.close();
+*/
 
+
+   Scanner odczyt = new Scanner(System.in);
+   int odlicznie;
+        System.out.println("Podaj cas odliczania: ");
+        for(odlicznie = odczyt.nextInt(); odlicznie>0; odlicznie--) {
+            System.out.println("Wybuch za: " + odlicznie);
+        }
+            System.out.println("BUM");
+        }
     }
-}
+
 
 
      /*   System.out.println("Ania");
